7daf44587b999b96a784228264c27c1c82548e58
Elnatan Derech <elnatand@tikalk.com>
2019-11-14 12:14:56 +0200

    no message

--- app/src/main/java/com/android/tikalarcorefuse/createroom/CreateRoomFragment.kt
+++ app/src/main/java/com/android/tikalarcorefuse/createroom/CreateRoomFragment.kt
@@ -7,18 +7,20 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.android.tikalarcorefuse.R
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_create_room.*
 
 class CreateRoomFragment : Fragment() {
 
     val db = FirebaseFirestore.getInstance()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(
-            R.layout.fragment_create_room, container, false
-        )
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val view: View = inflater.inflate(R.layout.fragment_create_room, container, false)
+
+
+        btnDone.setOnClickListener{
+
+        }
+        return view
     }
 }
