d2a84d1bd1356f3b79ea20db1f8347eda05c60d7
Ronel Girat <r.girat@gmail.com>
2019-11-14 09:26:50 +0200

    Initial commit

---
+++ app/src/main/java/com/android/tikalarcorefuse/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.android.tikalarcorefuse
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.sceneform.ux.ArFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        (arFragment as ArFragment).setOnTapArPlaneListener { hitResult, plane, motionEvent ->
+        }
+
+    }
+}
