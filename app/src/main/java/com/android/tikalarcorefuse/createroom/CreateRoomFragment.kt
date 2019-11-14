8e694fb6ec1d979ef659223c301c9dbdd7f219b1
Elnatan Derech <elnatand@tikalk.com>
2019-11-14 12:28:25 +0200

    no message

--- app/src/main/java/com/android/tikalarcorefuse/createroom/CreateRoomFragment.kt
+++ app/src/main/java/com/android/tikalarcorefuse/createroom/CreateRoomFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.android.tikalarcorefuse.R
+import com.android.tikalarcorefuse.data.Room
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_create_room.*
 
@@ -19,6 +20,9 @@ class CreateRoomFragment : Fragment() {
 
 
         btnDone.setOnClickListener{
+            val room =Room()
+            room.name=etName.text.toString()
+            room.numOfUser=etNumOfUsers.text.toString().toInt()
 
         }
         return view
